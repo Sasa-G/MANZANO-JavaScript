30bfe7a2a9e4b71409de02a8d1572c54d3148636
Samanta  Santos <142459272+Sasa-G@users.noreply.github.com>
2024-04-18 00:35:24 -0300

    Add files via upload

---
+++ PAGINA 50/1A/1A.js
@@ -0,0 +1,9 @@
+/* Apresentar os quadrados dos números inteiros de 15 a 200.*/
+
+let soma;
+
+for (let numero = 0; numero <= 200; numero++) {
+    soma= numero*numero;
+    console.log("O quadrado de " + numero + " é  " + soma)
+    
+}
