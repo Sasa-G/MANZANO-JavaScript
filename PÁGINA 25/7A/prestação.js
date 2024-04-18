418b6950e4198a59407879240feedb1aa34be17e
Samanta  Santos <142459272+Sasa-G@users.noreply.github.com>
2024-04-17 22:35:30 -0300

    Add files via upload

---
+++ PÁGINA 25/7A/prestação.js
@@ -0,0 +1,8 @@
+/*Efetuar o cálculo e a apresentação do valor de uma prestação em atraso, utilizando a fórmula
+PRESTACAO  VALOR + (VALOR * TAXA/100) * TEMPO).*/
+
+let prestação,valor,taxa, tempo;
+
+valor=parseFloat(prompt("Digite o valor da prestação"));
+taxa=parseFloat(prompt("Digote o valor da taxa de atraso"));
+
